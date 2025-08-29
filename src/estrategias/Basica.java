1010e3e664366ea7dcb2338a18f98960dd0b50e1
Facundo Sanchez <facundo.sanchez@gba.gob.ar>
2025-08-29 09:13:28 -0300

    primer commit

---
+++ src/estrategias/Basica.java
@@ -0,0 +1,19 @@
+package estrategias;
+
+public class Basica extends Strategy{
+
+    @Override
+    public void onScannedRobot() {
+
+    }
+
+    @Override
+    public void onHitByBullet() {
+
+    }
+
+    @Override
+    public void onHitWall() {
+
+    }
+}
---
+++ src/estrategias/Strategy.java
@@ -0,0 +1,8 @@
+package estrategias;
+
+public abstract class Strategy {
+
+    public abstract void onScannedRobot();
+    public abstract void onHitByBullet();
+    public abstract void onHitWall();
+}
